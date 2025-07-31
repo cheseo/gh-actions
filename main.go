fa3a9b510055596bfc87ca427c2f883c88ad764b
cheseo <cheseo@example.com>
2025-07-31 11:51:32 +0545

    add name

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
+<<<<<<< Updated upstream
 import "fmt"
+=======
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func add(a, b int) int {
+	return a+b
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+	name := r.FormValue("name")
+	if name == "" {
+		name = "unknown"
+	}
+	fmt.Fprintf(w, "Hello %s!!\n", name)
+}
+>>>>>>> Stashed changes
 
 func main(){
 	fmt.Println("Hello, world!")
